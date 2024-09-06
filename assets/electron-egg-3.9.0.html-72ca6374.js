7a93d4c81328b5e0559ef8451f718a0524e58164
yu199195 <549477611@qq.com>
2024-09-06 08:42:06 +0000

    Deploying to gh-pages from @ dromara/dromara.github.io@97c7c3b4ee5ab6c1c60268662b68756da3d00a70 🚀

---
+++ assets/electron-egg-3.9.0.html-72ca6374.js
@@ -0,0 +1,19 @@
+import{_ as e,a as i}from"./electron-egg-3.10.0-1-08884f5a.js";import{_ as a,o as n,c as r,f as d}from"./app-b61402ca.js";const t="/assets/img/news/electron-egg-3.9.0-2.jpg",s="/assets/img/news/electron-egg-3.9.0-3.png",c={},l=d('<p>大家好，<code>electron-egg 3.9.0</code> 重大更新。</p><p>在这个版本里我们做了重大更新，支持了 <code>go</code> 语言，天生的跨平台、高性能，可以把核心业务用 go 来写，能极大提升了软件体验。</p><p>目前，框架已经广泛应用于记账、政务、企业、医疗、学校、股票交易、ERP、娱乐、视频等领域客户端，请放心使用！</p><figure><img src="'+e+`" alt="" tabindex="0"><figcaption></figcaption></figure><h3 id="为什么使用" tabindex="-1"><a class="header-anchor" href="#为什么使用" aria-hidden="true">#</a> 为什么使用</h3><p>桌面软件（办公方向、 个人工具），仍然是未来十几年 PC 端需求之一，提高工作效率</p><p>electron 技术是流行趋势，QQ、百度翻译、阿里网盘、迅雷、有道云笔记 ......</p><h3 id="开源" tabindex="-1"><a class="header-anchor" href="#开源" aria-hidden="true">#</a> 开源</h3><p>gitee：https://gitee.com/dromara/electron-egg 4200+</p><p>github：https://github.com/dromara/electron-egg 1300+</p><h3 id="本次更新" tabindex="-1"><a class="header-anchor" href="#本次更新" aria-hidden="true">#</a> 本次更新</h3><h3 id="_3-9-0" tabindex="-1"><a class="header-anchor" href="#_3-9-0" aria-hidden="true">#</a> 3.9.0</h3><p><strong>【重大更新】</strong></p><ol><li>【增加】新增 ee-go 模块，支持 go 语言，以及众多功能。</li><li>【增加】新增 ee-bin dev for go。</li><li>【增加】新增 ee-bin dev 配置 electron.loadingPage。</li><li>【增加】新增 ee-core cross 模块，支持跨语言服务。</li><li>【增加】新增 ee-core 生产环境配置 loadingPage。</li><li>【优化】优化 ee-bin dev --serve 支持自定义命令。</li><li>【优化】优化 ee-bin exec --cmds 支持自定义命令。</li><li>【优化】优化 ee-bin build --cmds 支持自定义命令。</li><li>【升级】升级 ee-core v2.7.1，升级 ee-bin v1.4.0</li></ol><h3 id="下载" tabindex="-1"><a class="header-anchor" href="#下载" aria-hidden="true">#</a> 下载</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code># gitee
+git clone https://gitee.com/dromara/electron-egg.git
+
+# github
+git clone https://github.com/dromara/electron-egg.git
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="安装" tabindex="-1"><a class="header-anchor" href="#安装" aria-hidden="true">#</a> 安装</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code># 设置国内镜像源(加速)
+npm config set registry=https://registry.npmmirror.com
+
+#如果下载electron慢，配置如下
+npm config set electron_mirror=https://registry.npmmirror.com/-/binary/electron/
+
+# 根目录，安装 electron 依赖
+npm i
+
+# 进入【前端目录】安装 frontend 依赖
+cd frontend
+npm i
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="运行项目" tabindex="-1"><a class="header-anchor" href="#运行项目" aria-hidden="true">#</a> 运行项目</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>npm run start
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h2 id="" tabindex="-1"><a class="header-anchor" href="#" aria-hidden="true">#</a> <img src="`+i+'" alt=""></h2><h3 id="用户案例" tabindex="-1"><a class="header-anchor" href="#用户案例" aria-hidden="true">#</a> 用户案例</h3><figure><img src="'+t+'" alt="" tabindex="0"><figcaption></figcaption></figure><figure><img src="'+s+'" alt="" tabindex="0"><figcaption></figcaption></figure><h3 id="更多" tabindex="-1"><a class="header-anchor" href="#更多" aria-hidden="true">#</a> 更多</h3><p>访问官网：https://www.kaka996.com/</p>',26),o=[l];function h(g,m){return n(),r("div",null,o)}const p=a(c,[["render",h],["__file","electron-egg-3.9.0.html.vue"]]);export{p as default};
