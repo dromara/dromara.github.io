4324b8250b8b801309f6d0ffd5c3cd1e1a5e7057
VampireAchao <achao@apache.org>
2024-10-13 15:21:53 +0800

    [Improve] preload jump local zh-CN

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -6,8 +6,21 @@ import VueViewer from "v-viewer";
 import "viewerjs/dist/viewer.css";
 
 export default defineClientConfig({
-  enhance ({ app }) {
+  enhance ({ app ,router }) {
     app.component("GiteeRepo", GiteeRepo);
     app.use(VueViewer);
+    router.beforeEach((to, from, next) => {
+      if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
+        next();
+        return;
+      }
+      const locale = navigator.language || navigator.userLanguage;
+      if (locale.startsWith("zh")) {
+        const url = "https://dromara.org.cn" + to.path;
+        window.location.href = url;
+      } else {
+        next();
+      }
+    });
   }
 });
