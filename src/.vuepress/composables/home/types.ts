9877f7b6675e58c22a02254aaed6656aaeae1ec6
tomsun28 <tomsun28@outlook.com>
2025-01-07 17:25:31 +0800

    add gitcode and incubator link

--- src/.vuepress/composables/home/types.ts
+++ src/.vuepress/composables/home/types.ts
@@ -24,6 +24,7 @@ interface CommunityItem {
 export interface HomeOption {
   QUICK_START: string
   DESCRIPTION: string
+  INCUBATOR: string
   FEATURES: Feature[]
   STARS_OVERALL: string
   DATA_SOURCE: string
